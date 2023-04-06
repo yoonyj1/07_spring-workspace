b85cc8a48a7697bc5b9d9806d2aa84d88d51bbf7
yoonyj1 <116607314+yoonyj1@users.noreply.github.com>
2023-04-06 11:30:05 +0900

    202304061130

--- springProject/src/main/java/com/kh/spring/common/interceptor/LoginInterceptor.java
+++ springProject/src/main/java/com/kh/spring/common/interceptor/LoginInterceptor.java
@@ -2,6 +2,7 @@ package com.kh.spring.common.interceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
@@ -21,7 +22,17 @@ public class LoginInterceptor extends HandlerInterceptorAdapter{
 		// true 리턴 => 기존 요청 흐름대로 해당 Controller 제대로 실행
 		// false 리턴 => Controller 실행되지 않음
 		
-		// 현재 요청을 보낸 사람이 로그인 되어있을 경우 => Controller 실행
+		HttpSession session = request.getSession();
+		
+		if(session.getAttribute("loginUser") != null) {
+			// 현재 요청을 보낸 사람이 로그인 되어있을 경우 => Controller 실행
+			return true;
+		} else {
+			session.setAttribute("alertMsg", "로그인 후 이용가능한 서비스입니다.");
+			
+			response.sendRedirect(request.getContextPath());
+			return false;
+		}
 		// 로그인 되어있지 않은 경우				=> Controller 실행 x
 	}
 }
